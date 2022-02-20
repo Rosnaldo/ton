2e0d16b74d733fc4053d0ef0858ee0bf3aaf5ab5
Andrey <andreytsuzuki@gmail.com>
2022-02-20 18:47:57 -0300

    fix: config database

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,3 +1,5 @@
-export default () => ({
+import { registerAs } from '@nestjs/config'
+
+export default registerAs('database', () => ({
   uri: process.env.MONGO_URI || 'mongodb://localhost:27017/test',
-})
+}))
