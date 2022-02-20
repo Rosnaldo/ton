9b13e20b5b6b618630312611dbc9f27b668d4e1a
Andrey <andreytsuzuki@gmail.com>
2022-02-20 18:21:21 -0300

    fix(config.local): remove whitelist

--- src/config/local.config.ts
+++ src/config/local.config.ts
@@ -1,5 +1,4 @@
 export default () => ({
   port: parseInt(process.env.PORT, 10) || 4002,
   domain: process.env.DOMAIN_REST || 'http://localhost:3001',
-  whitelist: [/https?:\/\/(localhost|192\.168\.[\d]{1,3}\.[\d]{1,3}):([34]00[0-2]|7004|80[09][0-2]|9000)/],
 })
