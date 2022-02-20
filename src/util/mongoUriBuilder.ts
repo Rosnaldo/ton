acd3891169bd35df25513757fccfe0c21c0c8fd5
Andrey <andreytsuzuki@gmail.com>
2022-02-20 16:10:38 -0300

    feat(util.mongoUriBuilder): remove || options

--- src/util/mongoUriBuilder.ts
+++ src/util/mongoUriBuilder.ts
@@ -1,5 +1,5 @@
 export default function mongoUriBuilder(params?) {
-  const db = params?.db || ''
-  const host = params?.host || ''
+  const db = params?.db
+  const host = params?.host
   return `mongodb://${host}/${db}`
 }
