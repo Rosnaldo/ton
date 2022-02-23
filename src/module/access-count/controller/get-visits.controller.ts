dcb736c7e091f5892498953a8e0be3afd7a660ce
Andrey <andreytsuzuki@gmail.com>
2022-02-23 01:00:43 -0300

    feat(AccessCount): return ResponseType

--- src/module/access-count/controller/get-visits.controller.ts
+++ src/module/access-count/controller/get-visits.controller.ts
@@ -2,13 +2,18 @@ import { Controller, Get } from '@nestjs/common'
 import { ApiTags } from '@nestjs/swagger'
 import { CountApiVisitsService } from '../service/count-api'
 
+type ReponseType = {
+  status: number
+  path: string
+  value: number
+}
+
 @ApiTags('access-count')
 @Controller('access-count')
 export class AccessCountGetVisitsController {
   constructor(private readonly countApiVisitsService: CountApiVisitsService) {}
   @Get()
-  async handle(): Promise<number> {
-    const result = await this.countApiVisitsService.get()
-    return result.value
+  async handle(): Promise<ReponseType> {
+    return this.countApiVisitsService.get()
   }
 }
--- src/module/access-count/controller/hit-visits.controller.ts
+++ src/module/access-count/controller/hit-visits.controller.ts
@@ -2,12 +2,18 @@ import { Controller, Post } from '@nestjs/common'
 import { ApiTags } from '@nestjs/swagger'
 import { CountApiVisitsService } from '../service/count-api'
 
+type ReponseType = {
+  status: number
+  path: string
+  value: number
+}
+
 @ApiTags('access-count')
 @Controller('access-count')
 export class AccessCountHitVisitsController {
   constructor(private readonly countApiVisitsService: CountApiVisitsService) {}
   @Post('hit')
-  async handle(): Promise<any> {
+  async handle(): Promise<ReponseType> {
     return this.countApiVisitsService.hit()
   }
 }
